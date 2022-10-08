44b21726d133db38a3bda47ac20aea719bc1bdfc
Patryk Kaiser <73563195+Telephonelines@users.noreply.github.com>
2022-10-08 22:51:05 +0100

    Added Kruskal algorithm
    Kruskal's algorithm using Edge objects

---
+++ src/Kruskal.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class Kruskal {
+    Graph g;
+
+    public Kruskal(Graph g) {
+        this.g = g;
+    }
+
+    public Graph mst() {
+        QuickUnionUFWeighting set = new QuickUnionUFWeighting(g.size());
+        int i = 0;
+        Edge[] ordered_edge = new Edge[g.countEdges()];
+        System.out.println(g.countEdges());
+        Graph mst = new Graph(g.size());
+        for (LinkedList<Edge> e_list : g.getGraph()) {
+            for (Edge e : e_list) {
+                ordered_edge[i++] = e;
+            }
+        }
+        // Sort the edges by weight
+        for (Edge e : ordered_edge) {
+            Arrays.sort(ordered_edge, e.WEIGHT_ORDER);
+        }
+        // Create the MST graph
+        for (Edge e : ordered_edge) {
+            if (set.find(e.from()) != set.find(e.to())) {
+                mst.addEdge(e.from(), e.to(), e.weight());
+                set.union(e.from(), e.to());
+            }
+        }
+        return mst;
+    }
+}
